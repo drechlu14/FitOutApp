4b606649aa583e8f90fd745153fb8460fabe3645
drechlu14 <drechlu14@DST20708>
2017-05-23 09:26:09 +0200

    new page progress

---
+++ FitOutApp/FitOutApp/FitOutApp/Views/ProgressPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using FitOutApp.Models;
+using FitOutApp.ViewModels;
+
+using Xamarin.Forms;
+
+namespace FitOutApp.Views
+{
+    public partial class ProgressPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ProgressPage()
+        {
+            InitializeComponent();
+
+
+
+        }
+       
+    }
+}
